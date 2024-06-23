ffefe95cecc360545ae0858f1c681302106eb56b
Yiyang Wu <toolmanp@tlmp.cc>
2024-06-23 17:16:58 +0800

    feat: add formatter

--- lua/editor/plugins/formatter.lua
+++ lua/editor/plugins/formatter.lua
@@ -14,17 +14,15 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { "isort", "black" },
-        javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        json = { 'prettier' },
+        jsonc = { 'prettier' },
+        yaml = { 'prettier' },
+        css = { 'prettier' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
       },
     },
   },
