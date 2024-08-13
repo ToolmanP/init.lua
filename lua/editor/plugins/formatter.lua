4c7ba9f6579b227dd24d0ee3cfc5272b38610982
Yiyang Wu <toolmanp@tlmp.cc>
2024-08-13 11:51:08 +0800

    feat: add uncrustify local format
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- lua/editor/plugins/formatter.lua
+++ lua/editor/plugins/formatter.lua
@@ -1,3 +1,16 @@
+local find_uncrustify_config = function()
+  local cwd = vim.fn.expand '%:p:h'
+  local config = vim.fs.find('uncrustify.cfg', {
+    path = cwd,
+    upward = true,
+    limit = math.huge,
+  })
+  if config == '' then
+    return nil
+  end
+  return config[1]
+end
+
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
@@ -12,20 +25,34 @@ return {
         desc = '[F]ormat buffer',
       },
     },
-    opts = {
-      notify_on_error = false,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        javascript = { { 'prettierd', 'prettier' } },
-        json = { 'prettier' },
-        jsonc = { 'prettier' },
-        yaml = { 'prettier' },
-        css = { 'prettier' },
-        c = { 'clang-format' },
-        cpp = { 'clang-format' },
-        python = { 'isort', 'black' },
-        sh = { 'shfmt' },
-      },
-    },
+    config = function()
+      local opts = {
+        notify_on_error = false,
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          javascript = { { 'prettierd', 'prettier' } },
+          json = { 'prettier' },
+          jsonc = { 'prettier' },
+          yaml = { 'prettier' },
+          css = { 'prettier' },
+          c = function(bufnr)
+            local uncrustify_config = find_uncrustify_config()
+            local conform = require 'conform'
+            if conform.get_formatter_info('uncrustify', bufnr).available and uncrustify_config then
+              conform.formatters.uncrustify = {
+                prepend_args = { '-c', uncrustify_config },
+              }
+              return { 'uncrustify' }
+            else
+              return { 'clang-format' }
+            end
+          end,
+          cpp = { 'clang-format' },
+          python = { 'isort', 'black' },
+          sh = { 'shfmt' },
+        },
+      }
+      require('conform').setup(opts)
+    end,
   },
 }
