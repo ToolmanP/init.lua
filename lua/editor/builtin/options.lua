58f004fa62c74a937ebf93f8e4d8c276a86f4c1e
Yiyang Wu <toolmanp@tlmp.cc>
2024-08-30 22:19:43 +0800

    clipboard: revert to unnamedplus
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- lua/editor/builtin/options.lua
+++ lua/editor/builtin/options.lua
@@ -84,16 +84,5 @@ return {
     opt.hlsearch = true
 
     opt.clipboard = 'unnamedplus'
-    g.clipboard = {
-      name = 'OSC 52',
-      copy = {
-        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-      },
-      paste = {
-        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-      },
-    }
   end,
 }
