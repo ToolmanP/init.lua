45897810f7bed47f7fa60876c1a5ea67c822b26b
toolmanp <toolmanp@tlmp.cc>
2025-10-02 21:28:43 +0800

    feat(lsp): override original keymaps
    Signed-off-by: toolmanp <toolmanp@tlmp.cc>

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,22 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local ndel = require("utils").ndel
+local nmap = require("utils").nmap
+
+ndel "grr"
+ndel "gri"
+
+nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+nmap("gp", require("telescope.builtin").diagnostics, "[G]oto [P]roblems")
+
+nmap("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+nmap("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+nmap("<leader>Ld", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+nmap("<leader>Ls", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+nmap("<leader>Lw", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+local servers = { "html", "cssls", "clangd", "luals", "gopls", "basedpyright", "bashls", "copilot" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
