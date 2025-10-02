ca77f0736791f251a788cb139fbc90efa7f379c2
toolmanp <toolmanp@tlmp.cc>
2025-10-02 21:27:18 +0800

    feat(mappings): add keymappings
    Signed-off-by: toolmanp <toolmanp@tlmp.cc>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,32 @@
 require "nvchad.mappings"
 
 -- add yours here
+--
 
 local map = vim.keymap.set
+local keydel = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("n", "<leader>de", vim.diagnostic.open_float, { desc = "Show diagnostic Error messages" })
+map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "Open diagnostic Quickfix list" })
+map("n", "<A-Tab>", "<CMD>tabnext<CR>", { desc = "[T]ab Next" })
+map("n", "H", require("nvchad.tabufline").prev, { desc = "Buffer Prev" })
+map("n", "L", require("nvchad.tabufline").next, { desc = "Buffer Next" })
 
+map("n", "<leader>fB", "<CMD>Telescope file_browser<CR>", { desc = "File Browser" })
+map(
+  "n",
+  "<leader>fl",
+  "<CMD>Telescope file_browser path=%:p:h select_buffer=true<CR>",
+  { desc = "Local File Browser" }
+)
+
+map(
+  "n",
+  "<leader>fd",
+  "<CMD>Telescope file_browser files=false depth=false<CR>",
+  { desc = "Recursive Directories Browser" }
+)
+
+keydel("n", "<leader>v")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
